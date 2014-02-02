3b3fe201d29278d2109aa6e3b41ec4a6ced6fa8c
Daniel Neal <danielneal2@gmail.com>
2014-02-02 22:26:46 +0000

    Convert datomic entity to maps with renamed keys

--- src/server/core.clj
+++ src/server/core.clj
@@ -8,6 +8,7 @@
     [compojure.handler :refer [api]]
     [compojure.route :refer [resources files]]
     [datomic.api :as d :refer [q]]))
+   [clojure.set :as set]))
 
 ;; -------------------------------
 ;;  Persistence
@@ -17,13 +18,19 @@
 (d/create-database uri)
 (def conn (d/connect uri))
 (d/transact conn (-> "schema.edn" slurp read-string))
+(defn hydrate [q]
+  (into #{} (map (comp (partial into {}) d/touch (partial d/entity (d/db conn)) first) q)))
 
 (comment (d/transact conn [{:feature/title "hello" :feature/votes 3 :db/id (d/tempid :db.part/user)}])
+(defn features-all []
+  (as-> (q '[:find ?e
+            :in $
+            :where [?e :feature/title ?f]] (d/db conn)) q
+       (hydrate q)
+       (set/rename q {:feature/votes :votes
+                    :feature/title :title
+                    :feature/description :description})))
 
-  (q '[:find ?f ?v
-       :in $
-       :where [?e :feature/title ?f]
-       [?e :feature/votes ?v]] (d/db conn)))
 
 ;; -------------------------------
 ;;     Message handling
