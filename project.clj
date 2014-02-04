501255596d6e54f1c0fefedbae95ae42172ac271
Daniel Neal <danielneal2@gmail.com>
2014-02-04 17:16:30 +0000

    Update to latest Om

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/clojurescript "0.0-2138"]
                  [org.clojure/core.async "0.1.267.0-0d7780-alpha"]
-                 [om "0.3.0"]
+                 [om "0.3.5"]
                  [com.datomic/datomic-free "0.9.4470"]
                  [jarohen/chord "0.2.2"]
                  [compojure "1.1.6"]
